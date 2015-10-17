7d9110bc439fbc958022643f1a912585df0826bf
John Chen <john.chen@rangle.io>
2015-10-17 17:12:00 -0400

    gitignore

--- dist/button/button.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-angular.module('magazine.button',[
-    'magazine.button.directive',
-    'magazine.button.controller'
-]);
--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,32 @@ var watch = require('gulp-watch');
 var changed = require('gulp-changed');
 var livereload = require('gulp-livereload');
 
-gulp.task('serve',
-            serve({
-                root: ['dist'],
-                port: 8000
-            })
+var ghPages = require('gulp-gh-pages');
 
+gulp.task('server',
+    serve({
+        root: ['dist'],
+        port: 8000
+    })
 );
 
-gulp.task('reload', function(){
+gulp.task('reload', function () {
     gulp.src('src/**/*')
         .pipe(changed('dist'))
         .pipe(gulp.dest('dist'))
         .pipe(livereload());
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     livereload.listen();
     gulp.watch(['src/**/*'], ['reload']);
 });
 
 
-gulp.task('default', ['serve', 'watch']);
+gulp.task('deploy', function () {
+    return gulp.src('./dist/**/*')
+        .pipe(ghPages());
+});
+
+
+gulp.task('serve', ['server', 'watch']);
